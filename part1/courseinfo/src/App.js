7f88aaf863d24c33656f1b7b8b598f19ba399f0f
Miguelón <lmiguelperezp@gmail.com>
2022-07-15 21:36:05 +0200

    part1.2.1 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,45 +1,57 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <>
-      <Header course={course} />
-      <Content text1={part1} number1={exercises1}
-        text2={part2} number2={exercises2}
-        text3={part3} number3={exercises3} />
-      <Total n1={exercises1} n2={exercises2} n3={exercises3} />
+      <Header course = { course.name } />
+      <Content values = { course.parts } />
+      <Total values = { course.parts} />
     </>
   )
 }
 
-const Header = (props) => (
+const Header = ( props ) => (
   <>
-    <h1>{props.course}</h1>
+    <h1>{ props.course }</h1>
   </>
 )
-const Content = (props) => {
-  console.log(props);
-  return (<>
-    <Part text={props.text1} number={props.number1} />
-    <Part text={props.text2} number={props.number2} />
-    <Part text={props.text3} number={props.number3} />
-  </>)
+const Content = ( props ) => {
+  let elements = [];
+  let i = 1;
+  props.values.map( a => {
+    elements.push(<Part key = {i} text={ a.name } number={ a.exercises } />)
+    i++
+    return null
+  } )
+  return elements;
 }
-const Part = (props) => (
+const Part = ( props ) => (
   <>
-    <p>{props.text} {props.number}</p>
+    <p>{ props.text } { props.number }</p>
   </>
 );
-const Total = (props) => (
-  <>
-    <p>Number of exercises {props.n1 + props.n2 + props.n3}</p>
-  </>
-)
+const Total = ( props ) => {
+  let sum = props.values.reduce(( acc,obj )=> acc+obj.exercises, 0);
+  return (<>
+    <p>Number of exercises { sum }</p>
+  </>)
+}
 
 export default App
